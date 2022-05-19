e3387f61664307dc65ff7ca9ef876907f5541e96
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-05-19 06:39:45 +0600

    Todo section modified

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 const Todo = () => {
+
+    const handleRegisterSubmit = async (event) => {
+        event.preventDefault()
+        const name = event.target.name.value
+        const description = event.target.description.value
+        console.log(name, description)
+    }
+
     return (
         <div className="flex justify-center items-center h-screen">
             <div class="card w-96 bg-neutral text-neutral-content">
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">Add a task!</h2>
-                    <input type="text" placeholder="name" class="input input-bordered w-full max-w-xs" />
-                    <textarea type="text" placeholder="Description" class="input input-bordered w-full max-w-xs" />
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Update</button>
-                    </div>
+                    <form onSubmit={handleRegisterSubmit}>
+                        <input type="text"  name="name" placeholder="name" class="input input-bordered w-full max-w-xs my-3" />
+                        <textarea type="text"  name="description" placeholder="Description" class="input input-bordered w-full max-w-xs" />
+                        <div class="card-actions justify-center">
+                            <button class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
